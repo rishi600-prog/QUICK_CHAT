54da4559701ed0c39ff0cc90609a9fc54b9d93ed
rishi600-prog <srishimohan600@gmail.com>
2024-07-31 03:01:41 +0530

    Update

---
+++ server/cloudinary.js
@@ -0,0 +1,10 @@
+import Cloudinary from "cloudinary";
+const cloudinary = Cloudinary.v2;
+
+cloudinary.config({
+    cloud_name: "dwgckpez3",
+    api_key: "737318149466483",
+    api_secret: "T74T4s8Hqj6iQiEAsF1mRXRMmlY",
+});
+
+export default cloudinary;
