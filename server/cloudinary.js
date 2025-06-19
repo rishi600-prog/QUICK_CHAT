625120b9dbab514f79678978c5cd060d1b7d977d
rishi_s_mohan <srishimohan600@gmail.com>
2025-06-19 16:32:51 +0530

    Refactor Cloudinary configuration to use environment variables

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -2,9 +2,9 @@ import Cloudinary from "cloudinary";
 const cloudinary = Cloudinary.v2;
 
 cloudinary.config({
-    cloud_name: "dwgckpez3",
-    api_key: "737318149466483",
-    api_secret: "T74T4s8Hqj6iQiEAsF1mRXRMmlY",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
